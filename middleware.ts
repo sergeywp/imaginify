b54119cda07ff0f22f9b556843936d0b0661e6e2
Sergei Ponomarenko <sergei.ponomarenko@alogis.com>
2024-03-05 11:53:33 +0100

    Fix: try 3

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhooks/clerk'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/api/webhook/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk']
 });
  
 export const config = {
