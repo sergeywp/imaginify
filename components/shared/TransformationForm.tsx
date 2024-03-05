4bff3a01fa106f8a6401122c4c3c8015957d3951
Sergei Ponomarenko <sergei.ponomarenko@alogis.com>
2024-03-05 11:33:52 +0100

    Fix: error

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -50,27 +50,9 @@ const TransformationForm = ({
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <>
+    
+    </>
   );
 };
 
